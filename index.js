6cc33f1d8d89c81329625d1cee72a0681ad8b8bc
Anthony <anthony.guev@gmail.com>
2015-04-16 21:04:19 +1000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Zombie Apocalypse
+   version: 3
+   usage:   node zombie filename
+*/
 var http = require('http');
-var connect = require('connect')();
 var readline = require('readline');
 var fs = require('fs');
 var lineNumber = 0;
 var fileName = "";
 
+//Argument position from command line
 var Arguments = {
    NODE:     0,
    PATH:     1,
    FILENAME: 2
 }
 
+//What to read on specific file number
 var FileLine = {
    AREA_DIMENSIONS: 1,
    ZOMBIE_COORD:    2,
@@ -18,8 +25,10 @@ var FileLine = {
    ZOMBIE_MOVELIST: 4
 }
 
+//Maxiumum number of lines to read from an input file
 var LINES_LIMIT = 4;
 
+//Items read from an input file
 var readFile = {
    worldDimension     : { N: 0 },
    initPositionZombie : [],
@@ -28,11 +37,13 @@ var readFile = {
    loaded             : false
 };
 
+//Store co-ordinates
 function cord_xy (x, y) {
    this.x = x;
    this.y = y;
 }
 
+//Store co-ordinates in addition to zombie attributes
 function zmove (x, y) { 
    cord_xy.call(this, x, y);
    this.moveSequence = 0;
@@ -41,29 +52,34 @@ function zmove (x, y) {
 zmove.prototype = new cord_xy();
 zmove.prototype.constructor = zmove;
 
+//Read command line arguments
 var args = function processArguments() {
-   console.log("Processing arguments");
    fileName = process.argv[Arguments.FILENAME];
+   if (!fileName) {
+      console.log("Error: A filename must be passed as a parameter.");
+      console.log("Usage: node index filename");
+      process.exit();
+   }
 }();
 
+//Attributes for reading an input file
 var rd = readline.createInterface({
-    input: fs.createReadStream('./dicks.txt'),
+    input: fs.createReadStream('./' + fileName),
     output: process.stdout,
     terminal: false
 });
 
+//Read line by line of input file
 rd.on('line', function(line) {
    ++lineNumber;
    switch (lineNumber) {
       case FileLine.AREA_DIMENSIONS:
          readFile.worldDimension.N = parseInt(line);
-         console.log(readFile.worldDimension.N);
          break;
       case FileLine.ZOMBIE_COORD:
          var xy = line.split(" ");
          var cords = new zmove( parseInt(xy[0]), parseInt(xy[1]) );
          readFile.initPositionZombie.push(cords);;
-         console.log(readFile.initPositionZombie);
          break;
       case FileLine.CREATURE_COORD:
          var cords = line.split(",");
@@ -72,66 +88,75 @@ rd.on('line', function(line) {
             var x = parseInt(xy[0]); var y = parseInt(xy[1]);
             readFile.creaturePositions.push(new cord_xy(x, y));
          });
-         console.log(readFile.creaturePositions);
          break;
       case FileLine.ZOMBIE_MOVELIST:
          for (x = 0; x < line.length; ++x) {
             readFile.zombieMoveList.push(line[x]);
          }
-         console.log(readFile.zombieMoveList);
          break;
    }
-   if (lineNumber >= 4) { //all lines have been read
+   if (lineNumber >= LINES_LIMIT) { //all lines have been read
       readFile.loaded = true;
-      var worldMap = new WorldMap(readFile); //create world map
+      createScenario(); //Begin execution
    }
 });
 
+
+//Start the scenario
+function createScenario () {
+   worldMap = new WorldMap(readFile);
+}
+
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    WorldMap initialzes variables read from file
+*/
 function WorldMap (readFile) {
    this.dimension = readFile.worldDimension.N;
    this.zombies   = readFile.initPositionZombie;
    this.creatures = readFile.creaturePositions;
    this.movement  = readFile.zombieMoveList;
    this.score     = 0;
-   this.initBoard();
+   this.startScenario();//begin zombie apocalypse
 }
 
-WorldMap.prototype.initBoard = function() {
-   this.drawBoard();
-   this.displayBoard();
-
-   count = 0;
-   console.log("xxxxxxxxxxxxxxxxxxx");
-   console.log(count);
-   console.log(this.zombies.length);
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Begin the scenario by displaying the starting position of zombies
+            and creatures. Zombies move until movement sequence runs out.
+*/
+WorldMap.prototype.startScenario = function() {
+   console.log("[Begin Scenario]");
+   this.drawAndDisplayBoard();
+   var count = 0;
    while (count != this.zombies.length) {
       count = 0;
       for (var x = 0; x < this.zombies.length; ++x) {
-         console.log(this.zombies);
-         console.log("CAN MOVE?");
-         console.log(this.zombies[x].canMove);
          if (!this.zombies[x].canMove) {
             count++;
          }
       }
-      console.log("COUNTER:" + count);
       for (var i = 0; i < this.zombies.length && count != this.zombies.length; ++i) {
          this.stepZombie(i);
-         if ( this.zombieBite(i) ) {
-            this.zombies.push( new zmove(this.zombies[i].x, this.zombies[i].y) );
-            console.log("vbitepost");
-         }
-         else {
-            console.log("whitebite");
-         }
-         this.drawBoard();
-         console.log("after draw board");
-         this.displayBoard();
       }
    }
-   console.log(this.zombies);
+   this.endScenario();
 }
 
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Draw board and display board with most current updates
+*/
+WorldMap.prototype.drawAndDisplayBoard = function () {
+   this.drawBoard();
+   this.displayBoard();
+}
+
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Begin the scenario by displaying the starting position of zombies
+            and creatures. Zombies move until movement sequence runs out.
+*/
 WorldMap.prototype.drawBoard = function () {
    this.board = new Array(this.dimension);
    for (var x = 0; x < this.dimension; ++x)
@@ -139,11 +164,12 @@ WorldMap.prototype.drawBoard = function () {
    var board = this.board;
    var zombies = this.zombies;
    var creatures = this.creatures;
-   //fill board
+   //fill board with '-'
    for(row = 0; row < this.dimension; ++row)
       for(col = 0; col < this.dimension; ++col)
          board[col][row] = "-";
 
+   //add the zombies and creatures to the board
    drawZombies();
    drawCreatures();
 
@@ -158,8 +184,11 @@ WorldMap.prototype.drawBoard = function () {
    }
 }
 
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Display the board using ascii characters
+*/
 WorldMap.prototype.displayBoard = function() {
-
    for(row = 0; row < this.dimension; ++row) { //row
       for(col = 0; col < this.dimension; ++col) { //column
         process.stdout.write(this.board[row][col]); 
@@ -168,6 +197,11 @@ WorldMap.prototype.displayBoard = function() {
    }
 }
 
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Move the zombie based on movement list read from input file
+   param:   i - the index of the zombie to move
+*/
 WorldMap.prototype.stepZombie = function(i) {
    if (this.zombies[i].moveSequence == this.movement.length) {
       this.zombies[i].canMove = false;
@@ -180,86 +214,78 @@ WorldMap.prototype.stepZombie = function(i) {
       case "U"://up
          if ((this.zombies[i].y - 1) > -1) {
             this.zombies[i].y -= 1;
-            console.log("up");
-            console.log(this.zombies);
          }
          break;
       case "R"://right
          if ((this.zombies[i].x + 1) < this.dimension) {
             this.zombies[i].x += 1;
-            console.log("right");
-            console.log(this.zombies);
          }
          break;
       case "D"://down
+         if ((this.zombies[i].y + 1) < this.dimension) {
             this.zombies[i].y += 1;
-            console.log("down");
-            console.log(this.zombies);
+         }
          break;
       case "L"://left
       if ((this.zombies[i].x - 1) > -1) {
          this.zombies[i].x -= 1;
-         console.log("left");
-         console.log(this.zombies);
-      }
-      else {
-         console.log("Zombies can't go through walls!");
       }
       break;
    }
    this.zombies[i].moveSequence += 1;
-   //console.log(this.zombies[i]);
-   //console.log(this.zombies[i].moveSequence);
-   //console.log(this.movement.length);
-   //console.log(this.zombies[i].canMove);
-   //this.zombieBite(i);
-   //var x = new cord_xy(4,4)
-   //this.zombies.push(x);
+   this.checkBite(i);
 }
 
-WorldMap.prototype.zombieBite = function(i) {
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Check to see if zombie lands on same co-ordinate as a creature.
+   params:  i  -  index of the zombie to check for bite
+   return:  true  - zombie bite
+            false - no bite
+*/
+WorldMap.prototype.checkBite = function(i) {
    zx = this.zombies[i].x;
    zy = this.zombies[i].y;
    for (var x = 0; x < this.creatures.length; ++x) {
       if (zx == this.creatures[x].x &&
           zy == this.creatures[x].y) {
-         console.log("Zombie has bitten a poor creature!");
+         this.score++;
          var creatureDeathIndex = -1;
          this.creatures.forEach(function(creature, i) {
             if (creature.x == zx && creature.y == zy) {
                creatureDeathIndex = i;
             }
          });
-         console.log("A critter has been slain!");
-         console.log(this.creatures[creatureDeathIndex]);
          this.creatures.splice(creatureDeathIndex, 1);
-         //this.spawnZombie(zx, zy);
+         this.spawnZombie(zx, zy);
          return true;
       }
    }
    return false;
 }
 
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Create a new zombie if a creature receives a bite
+   params:  zx  -  the x co-ordinate of a zombie
+            zy  -  the y co-ordinate of a zombie
+*/
 WorldMap.prototype.spawnZombie = function(zx, zy) {
-   console.log("new zombie has spawned!");
    this.zombies.push(new zmove(zx, zy));
-   console.log(this.zombies);
 }
 
-function createScenario () {
-   var worldMap = new WorldMap(readFile)
+/* author:  Anthony Guevara
+   date:    16 April 2015
+   info:    Print results of scenario and exit
+*/
+WorldMap.prototype.endScenario = function () {
+   console.log("[End Scenario]");
+   this.drawAndDisplayBoard();
+   for (var i = 0; i < this.zombies.length; ++i) {
+      process.stdout.write(this.zombies[i].x + "," + this.zombies[i].y + " ");
+   }
+   process.stdout.write('\n');
+   console.log("Zombies weren't allowed to go through walls in this scenario.");
+   console.log("The scenario has ended");
+   process.exit();
 }
-
-connect.use("/", function(req,res) {
-   res.setHeader("Content-type", "application/json");
-   res.write(JSON.stringify({score:3,
-            babes:4
-            }));
-});
-
-var server = http.createServer(connect);
-
-server.listen(3000, function() {
-   console.log("Server listenning on port 3000)");
-});
-
